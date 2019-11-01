41add0d1c6708ea696d294ead345b886c8ee1dc8
Yehao Wei <52388763+Wyhzmgsx1552695@users.noreply.github.com>
2019-11-01 16:00:26 +0800

    Add files via upload

---
+++ volatileTest.java
@@ -0,0 +1,39 @@
+import static java.lang.Thread.sleep;
+
+public class volatileTest extends Thread{
+
+    public   static volatile int  n  =   0 ;
+    public static synchronized void inc(){
+        n++;
+    }
+    public    void  run()
+    {
+        for  ( int  i  =   0 ; i  <   10 ; i ++ )
+            try
+            {
+                //inc();
+                n++;
+                sleep( 3 );  //  为了使运行结果更随机，延迟3毫秒
+
+            }
+            catch  (Exception e)
+            {
+            }
+    }
+
+    public   static   void  main(String[] args)  throws  Exception
+    {
+
+        Thread threads[]  =   new  Thread[ 100 ];
+        for  ( int  i  =   0 ; i  <  threads.length; i ++ )
+            //  建立100个线程
+            threads[i]  =   new  volatileTest();
+        for  ( int  i  =   0 ; i  <  threads.length; i ++ )
+            //  运行刚才建立的100个线程
+            threads[i].start();
+        for  ( int  i  =   0 ; i  <  threads.length; i ++ )
+            //  100个线程都执行完后继续
+            threads[i].join();
+        System.out.println( " n= "   +  volatileTest.n);
+    }
+}
